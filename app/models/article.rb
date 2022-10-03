b96b16aa1445ac91c89cd2534eb517994a718a6b
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-10-04 03:45:01 +0530

    Added slug to article.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,33 @@
 
 class Article < ApplicationRecord
   validates :title, :body, :status, presence: true
+  validates :slug, uniqueness: true
+  validate :slug_not_changed
+
+  before_create :set_slug
+
+  private
+
+    def set_slug
+      title_slug = title.parameterize
+      latest_article_slug = Article.where(
+        "slug LIKE ? or slug LIKE ?",
+        "#{title_slug}",
+        "#{title_slug}-%"
+      ).order("LENGTH(slug) DESC", slug: :desc).first&.slug
+      slug_count = 0
+      if latest_article_slug.present?
+        slug_count = latest_article_slug.split("-").last.to_i
+        only_one_slug_exists = slug_count == 0
+        slug_count = 1 if only_one_slug_exists
+      end
+      slug_candidate = slug_count.positive? ? "#{title_slug}-#{slug_count + 1}" : title_slug
+      self.slug = slug_candidate
+    end
+
+    def slug_not_changed
+      if slug_changed? && self.persisted?
+        errors.add(:slug, t("task.slug.immutable"))
+      end
+    end
 end
