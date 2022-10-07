0f090a1074ecd83e9c0c22a8979de52d5cabdc34
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-10-08 02:19:09 +0530

    Fixed navbar button colours according to selected entity.

--- app/javascript/src/components/Common/NavBar.jsx
+++ app/javascript/src/components/Common/NavBar.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { ExternalLink } from "neetoicons";
 import { Typography, Button } from "neetoui";
+import { Link } from "react-router-dom";
 
 import { LANDING_PAGE_PATH, SETTING_PATH } from "components/routeConstants";
 
-const NavBar = () => (
-  <nav className="shadow flex h-16 border-b-2 bg-white px-6">
-    <div className="flex w-full items-center justify-between">
-      <div className="flex">
-        <Typography style="h4">Scribble</Typography>
-        <Button
-          className="mx-6"
-          label="Articles"
-          style="link"
-          to={LANDING_PAGE_PATH}
-        />
-        <Button label="Settings" style="link" to={SETTING_PATH} />
+const NavBar = () => {
+  const [articlesActiveStatus, setArticlesActiveStatus] = useState(true);
+
+  return (
+    <nav className="shadow flex h-16 border-b-2 bg-white px-6">
+      <div className="flex w-full items-center justify-between">
+        <div className="flex">
+          <Typography style="h4">Scribble</Typography>
+          <Link to={LANDING_PAGE_PATH}>
+            <Typography
+              style="h4"
+              className={
+                articlesActiveStatus
+                  ? "neeto-ui-text-primary-500 mx-6"
+                  : "neeto-ui-text-gray-500 mx-6"
+              }
+              onClick={() => setArticlesActiveStatus(true)}
+            >
+              Articles
+            </Typography>
+          </Link>
+          <Link to={SETTING_PATH}>
+            <Typography
+              style="h4"
+              className={
+                articlesActiveStatus
+                  ? "neeto-ui-text-gray-500"
+                  : "neeto-ui-text-primary-500"
+              }
+              onClick={() => setArticlesActiveStatus(false)}
+            >
+              Settings
+            </Typography>
+          </Link>
+        </div>
+        <Button icon={ExternalLink} label="preview" style="secondary" />
       </div>
-      <Button icon={ExternalLink} label="preview" style="secondary" />
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default NavBar;
