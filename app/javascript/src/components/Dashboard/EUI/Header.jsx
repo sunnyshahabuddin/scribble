2832d2e2e1dea0b3b4c4d0b407899781b84c8c03
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-10-14 03:47:25 +0530

    Added SideBar to list all categories and articles.

---
+++ app/javascript/src/components/Dashboard/EUI/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Typography } from "neetoui";
+
+const Header = () => (
+  <div className="flex h-12 w-full justify-center border-b-2 py-3">
+    <Typography style="h4">Spinkart</Typography>
+  </div>
+);
+
+export default Header;
