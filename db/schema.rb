9162b256ae3cd93c7d0196affc519df39de29f1b
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-10-16 10:52:50 +0530

    Added redirection model.

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_16_002025) do
+ActiveRecord::Schema.define(version: 2022_10_16_051808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2022_10_16_002025) do
     t.integer "position"
     t.integer "user_id", default: 1, null: false
     t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
+  create_table "redirections", force: :cascade do |t|
+    t.string "to"
+    t.string "from"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
