915a95e010a551db056e25e12cb7faf6ad502703
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-10-17 04:01:43 +0530

    Added tests for website model. (#114)

--- app/models/website.rb
+++ app/models/website.rb
@@ -4,4 +4,5 @@ class Website < ApplicationRecord
   validates :name, presence: true
   has_secure_password validations: false
   has_secure_token :authentication_token
+  validates :password, length: { minimum: 6 }, if: -> { password.present? }
 end
--- test/models/website_test.rb
+++ test/models/website_test.rb
@@ -6,4 +6,49 @@ class WebsiteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @website = create(:website)
+  end
+
+  def test_website_should_be_valid
+    assert @website.valid?
+  end
+
+  def test_website_shouldnt_be_valid_and_saved_without_name
+    @website.name = nil
+    assert @website.invalid?
+    assert_includes @website.errors.full_messages, "Name can't be blank"
+  end
+
+  def test_website_should_be_valid_without_password
+    @website.password = nil
+    assert @website.valid?
+  end
+
+  def test_website_shouldnt_be_valid_with_password_less_than_6_characters
+    @website.password = "pass1"
+    assert @website.invalid?
+    assert_includes @website.errors.full_messages, "Password is too short (minimum is 6 characters)"
+  end
+
+  def test_website_should_be_valid_with_password_more_than_6_characters
+    @website.password = "pass123"
+    assert @website.valid?
+  end
+
+  def test_website_should_be_valid_without_authentication_token
+    @website.authentication_token = nil
+    assert @website.valid?
+  end
+
+  def test_website_should_be_valid_with_authentication_token
+    @website.authentication_token = "token123"
+    assert @website.valid?
+  end
+
+  def test_website_shouldnt_be_nameless
+    @website.name = ""
+    assert @website.invalid?
+    assert_includes @website.errors.full_messages, "Name can't be blank"
+  end
 end
