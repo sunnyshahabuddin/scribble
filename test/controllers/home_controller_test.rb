4fbdd93555137361441335c6a07cf247e53ee929
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-10-17 19:02:27 +0530

    Written test for home controller. (#123)

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,4 +6,8 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def test_should_get_successfully_from_root_url
+    get root_path
+    assert_response :success
+  end
 end
