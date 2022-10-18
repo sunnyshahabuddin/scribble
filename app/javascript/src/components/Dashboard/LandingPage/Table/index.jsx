c2d8584f64d1ca31b427f318011aad9e114365b9
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-10-18 21:06:17 +0530

    Added Pagination in table.

--- app/javascript/src/components/Dashboard/LandingPage/Table/index.jsx
+++ app/javascript/src/components/Dashboard/LandingPage/Table/index.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Table as NeetoUITable } from "neetoui";
 
 import { buildTableColumnData } from "./utils";
 
-const Table = ({ articles = [], destroyArticle }) => (
-  <NeetoUITable
-    columnData={buildTableColumnData(destroyArticle)}
-    rowData={articles}
-  />
-);
+const Table = ({ articles = [], destroyArticle }) => {
+  const [currentTablePageNumber, setCurrentTablePageNumber] = useState(1);
+
+  return (
+    <NeetoUITable
+      columnData={buildTableColumnData(destroyArticle)}
+      currentPageNumber={currentTablePageNumber}
+      defaultPageSize={10}
+      handlePageChange={e => setCurrentTablePageNumber(e)}
+      rowData={articles}
+    />
+  );
+};
 
 export default Table;
