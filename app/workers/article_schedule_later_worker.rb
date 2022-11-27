38b9026c3bf5ec05105b592f571785b45d0a0905
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-11-27 16:44:01 +0530

    Revamped the logic of invoking worker. (#281)

--- app/workers/article_schedule_later_worker.rb
+++ app/workers/article_schedule_later_worker.rb
@@ -4,8 +4,13 @@
    include Sidekiq::Worker
    sidekiq_options queue: :default, retry: 10
 
-   def perform(id, schedule_status)
-     article_schedule_later_service = Api::Admin::ArticleScheduleLaterService.new(id, schedule_status)
-     article_schedule_later_service.process
+   def perform
+     articles = Article.select { |article| article.schedule_at.present? && article.schedule_at <= Time.zone.now }
+
+     articles.each do |article|
+       puts article.schedule_at
+       article_schedule_later_service = Api::Admin::ArticleScheduleLaterService.new(article.id, article.schedule_status)
+       article_schedule_later_service.process
+     end
    end
  end
