4fc6499ae66bae495a7ed535e1e68f8460c1897b
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-12-09 03:06:56 +0530

    Added ability to drag and drop articles. (#326)

--- app/javascript/src/components/Dashboard/Settings/ManageCategories/ManageArticles/Article.jsx
+++ app/javascript/src/components/Dashboard/Settings/ManageCategories/ManageArticles/Article.jsx
@@ -2,59 +2,78 @@ import React, { useState } from "react";
 
 import { Clock } from "neetoicons";
 import { Typography, Tag, Avatar, Tooltip, Checkbox } from "neetoui";
+import { Draggable } from "react-beautiful-dnd";
 
 import {
   calculateUpdatedAtFromNow,
   formatDateToDayDateMonthYearTime,
 } from "components/Dashboard/utils";
 
-const Article = ({ article }) => {
+const Article = ({ article, index }) => {
   const [checkedValue, setCheckedValue] = useState(true);
 
   return (
-    <div className="neeto-ui-shadow-xs border w-full space-y-2 p-4">
-      <Checkbox
-        checked={checkedValue}
-        className="mt-3"
-        id="selectedArticle"
-        name="selectedArticle"
-        onChange={() => setCheckedValue(checkedValue => !checkedValue)}
-      />
-      <Typography style="h4">{article.title}</Typography>
-      <Typography className="neeto-ui-text-gray-600 truncate" style="body2">
-        {article.body}
-      </Typography>
-      <hr />
-      <div className="flex justify-end">
-        <div className="flex items-center justify-between">
-          <Clock color="#68737d" size="20" />
-          <Tooltip
-            content={formatDateToDayDateMonthYearTime(article.updatedAt)}
-            followCursor="horizontal"
-            position="bottom"
-          >
+    <Draggable
+      draggableId={article.id.toString()}
+      index={index}
+      key={article.id}
+    >
+      {provided => (
+        <div
+          className="my-4 flex items-center justify-between"
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <div className="neeto-ui-shadow-xs border w-full space-y-2 p-4">
+            <Checkbox
+              checked={checkedValue}
+              className="mt-3"
+              id="selectedArticle"
+              name="selectedArticle"
+              onChange={() => setCheckedValue(checkedValue => !checkedValue)}
+            />
+            <Typography style="h4">{article.title}</Typography>
             <Typography
-              className="neeto-ui-text-gray-600 ml-1 mr-2"
-              style="body3"
+              className="neeto-ui-text-gray-600 truncate"
+              style="body2"
             >
-              {`${
-                article.status === 1 ? "Published " : "Drafted "
-              }${calculateUpdatedAtFromNow(article.updatedAt)}`}
+              {article.body}
             </Typography>
-          </Tooltip>
-          <Avatar
-            user={{
-              name: article.author.name,
-            }}
-          />
-          <Tag
-            className="neeto-ui-rounded-sm neeto-ui-bg-gray-100 ml-2 box-border h-6 px-2 py-px"
-            label={article.status === 1 ? "Published" : "Draft"}
-            style={article.status === 1 ? "success" : "warning"}
-          />
+            <hr />
+            <div className="flex justify-end">
+              <div className="flex items-center justify-between">
+                <Clock color="#68737d" size="20" />
+                <Tooltip
+                  content={formatDateToDayDateMonthYearTime(article.updatedAt)}
+                  followCursor="horizontal"
+                  position="bottom"
+                >
+                  <Typography
+                    className="neeto-ui-text-gray-600 ml-1 mr-2"
+                    style="body3"
+                  >
+                    {`${
+                      article.status === 1 ? "Published " : "Drafted "
+                    }${calculateUpdatedAtFromNow(article.updatedAt)}`}
+                  </Typography>
+                </Tooltip>
+                <Avatar
+                  user={{
+                    name: article.author.name,
+                  }}
+                />
+                <Tag
+                  className="neeto-ui-rounded-sm neeto-ui-bg-gray-100 ml-2 box-border h-6 px-2 py-px"
+                  label={article.status === 1 ? "Published" : "Draft"}
+                  style={article.status === 1 ? "success" : "warning"}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </Draggable>
   );
 };
 
