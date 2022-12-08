b8e6d3aef58c07a7baa04b85ec020aa21964b7ce
Sunny Shahabuddin <43422638+sunnyshahabuddin@users.noreply.github.com>
2022-12-09 03:40:51 +0530

    Added alert to move articles to category. (#330)

--- app/javascript/src/components/Dashboard/Settings/ManageCategories/ManageArticles/index.jsx
+++ app/javascript/src/components/Dashboard/Settings/ManageCategories/ManageArticles/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-import { Select } from "neetoui";
+import { Select, Alert } from "neetoui";
 import { Container, Header, Scrollable } from "neetoui/layouts";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
@@ -12,6 +12,8 @@ import InstructionsCallout from "./InstructionsCallout";
 const ManageArticles = ({ articles, setArticles, categoryList }) => {
   const [showInstructions, setShowInstructions] = useState(false);
   const [selectedArticlesIds, setSelectedArticlesIds] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
+  const [moveToCategory, setMoveToCategory] = useState({});
 
   const handleShowInstructions = () => {
     const showInstructionsFromLocalStorage = JSON.parse(
@@ -22,6 +24,11 @@ const ManageArticles = ({ articles, setArticles, categoryList }) => {
     } else {
       setShowInstructions(true);
     }
+  };
+
+  const handleMoveToCategory = category => {
+    setShowAlert(true);
+    setMoveToCategory(category);
   };
 
   const reorder = (categoryList, startIndex, endIndex) => {
@@ -48,54 +55,66 @@ const ManageArticles = ({ articles, setArticles, categoryList }) => {
   }, [showInstructions]);
 
   return (
-    <Container isHeaderFixed className="fixed">
-      <Header
-        title="Manage Articles"
-        actionBlock={
-          <TooltipWrapper
-            content="Select at least an article first to move them into a category"
-            disabled={selectedArticlesIds.length === 0}
-            followCursor="horizontal"
-            position="bottom"
-          >
-            <Select
-              isSearchable
-              className="w-48"
-              isDisabled={selectedArticlesIds.length === 0}
-              name="category"
-              placeholder="Move to category"
-              options={categoryList.map(category => ({
-                label: category.name,
-                value: category.id,
-              }))}
-            />
-          </TooltipWrapper>
-        }
-      />
-      <Scrollable className="h-full w-full space-y-6 py-6" size="large">
-        {showInstructions && (
-          <InstructionsCallout setShowInstructions={setShowInstructions} />
-        )}
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="droppable">
-            {provided => (
-              <div {...provided.droppableProps} ref={provided.innerRef}>
-                {articles.map((article, index) => (
-                  <Article
-                    article={article}
-                    index={index}
-                    key={article.id}
-                    selectedArticlesIds={selectedArticlesIds}
-                    setSelectedArticlesIds={setSelectedArticlesIds}
-                  />
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </Scrollable>
-    </Container>
+    <>
+      <Container isHeaderFixed className="fixed">
+        <Header
+          title="Manage Articles"
+          actionBlock={
+            <TooltipWrapper
+              content="Select at least an article first to move them into a category"
+              disabled={selectedArticlesIds.length === 0}
+              followCursor="horizontal"
+              position="bottom"
+            >
+              <Select
+                isSearchable
+                className="w-48"
+                isDisabled={selectedArticlesIds.length === 0}
+                name="category"
+                placeholder="Move to category"
+                options={categoryList.map(category => ({
+                  label: category.name,
+                  value: category.id,
+                }))}
+                onChange={category => handleMoveToCategory(category)}
+              />
+            </TooltipWrapper>
+          }
+        />
+        <Scrollable className="h-full w-full space-y-6 py-6" size="large">
+          {showInstructions && (
+            <InstructionsCallout setShowInstructions={setShowInstructions} />
+          )}
+          <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="droppable">
+              {provided => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {articles.map((article, index) => (
+                    <Article
+                      article={article}
+                      index={index}
+                      key={article.id}
+                      selectedArticlesIds={selectedArticlesIds}
+                      setSelectedArticlesIds={setSelectedArticlesIds}
+                    />
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </Scrollable>
+      </Container>
+      {showAlert && (
+        <Alert
+          isOpen={showAlert}
+          message={`Are you sure you want to move these ${selectedArticlesIds.length} selected articles to ${moveToCategory.label}?`}
+          title="Move Articles"
+          onClose={() => setShowAlert(false)}
+          onSubmit={() => setShowAlert(false)}
+        />
+      )}
+    </>
   );
 };
 
